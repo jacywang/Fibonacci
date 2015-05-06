8108ccb2366bf8615e7e7624a1dc6a4fabb4495d
Jacy Wang <jacywj@gmail.com>
2015-05-06 16:43:13 -0700

    V2

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -8,16 +8,35 @@
 
 #include <stdio.h>
 
+int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber); // Version Two
+
+
 int main(int argc, const char * argv[]) {
     int n;
     printf("Enter nth:");
     scanf("%d", &n);
-
+    
+// Version Two
     if (n ==1) {
         printf("The first Fibonacci number is 0\n");
     } else if (n==2) {
         printf("The first Fibonacci number is 1\n");
+    } else {
+        int remainingSteps = n-2;
+        int previousNumber = 0;
+        int previousPreviousNumber =1;
         
+        int newNumber = recursiveValueFibonacci(remainingSteps, previousNumber, previousPreviousNumber);
+        printf("The %d th Fibonacci number is %d\n", n, newNumber);
+    }
+
+
+// Version One
+    /*
+    if (n ==1) {
+        printf("The first Fibonacci number is 0\n");
+    } else if (n==2) {
+        printf("The first Fibonacci number is 1\n");
     } else {
         int firstNumber = 0;
         int secondNumber = 1;
@@ -33,4 +52,19 @@ int main(int argc, const char * argv[]) {
         
         printf("The %d th Fibonacci number is %d\n", n, newNumber);
     }
+     */
+}
+
+// Version Two
+int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber) {
+    int newNumber = previousNumber + previousPreviousNumber;
+    if (remainingSteps == 0) {
+        return newNumber;
+    } else {
+        previousPreviousNumber = previousNumber;
+        previousNumber = newNumber;
+        remainingSteps--;
+        return recursiveValueFibonacci(remainingSteps, previousNumber, previousPreviousNumber);
+    }
+    
 }
