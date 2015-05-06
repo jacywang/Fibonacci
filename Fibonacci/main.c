dfa48c8475ac10f29e3f64f01a4dbae76d5f3d10
Jacy Wang <jacywj@gmail.com>
2015-05-06 16:32:01 -0700

    V1

---
+++ Fibonacci/main.c
@@ -0,0 +1,38 @@
+//
+//  main.c
+//  Fibonacci
+//
+//  Created by JIAN WANG on 5/6/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int n;
+    printf("Enter nth:");
+    scanf("%d", &n);
+    
+    if (n > 2) {
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int newNumber =0;
+        
+        int i =2;
+        while (i < n) {
+            newNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = newNumber;
+            i++;
+        }
+        
+        printf("The %d th Fibonacci number is %d\n", n, newNumber);
+    } else if (n ==1) {
+        printf("The first Fibonacci number is 0\n");
+    } else if (n==2) {
+        printf("The first Fibonacci number is 1\n");
+
+    }
+    
+    return 0;
+}
