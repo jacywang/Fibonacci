e4aa354cd8da1430bb11e7193ce9104bffa2a0e3
Jacy Wang <jacywj@gmail.com>
2015-05-06 16:49:49 -0700

    V3

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -8,15 +8,31 @@
 
 #include <stdio.h>
 
-int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber); // Version Two
-
+// int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber); // Version Two
+int recursiveValueFibonacci(int remainingSteps, int *previousNumber, int *previousPreviousNumber); // Version Three
 
 int main(int argc, const char * argv[]) {
     int n;
     printf("Enter nth:");
     scanf("%d", &n);
     
+// Version Three
+    if (n ==1) {
+        printf("The first Fibonacci number is 0\n");
+    } else if (n==2) {
+        printf("The first Fibonacci number is 1\n");
+    } else {
+        int remainingSteps = n-2;
+        int previousNumber = 0;
+        int previousPreviousNumber =1;
+        
+        int newNumber = recursiveValueFibonacci(remainingSteps, &previousNumber, &previousPreviousNumber);
+        printf("The %d th Fibonacci number is %d\n", n, newNumber);
+    }
+
+    
 // Version Two
+    /*
     if (n ==1) {
         printf("The first Fibonacci number is 0\n");
     } else if (n==2) {
@@ -29,7 +45,7 @@ int main(int argc, const char * argv[]) {
         int newNumber = recursiveValueFibonacci(remainingSteps, previousNumber, previousPreviousNumber);
         printf("The %d th Fibonacci number is %d\n", n, newNumber);
     }
-
+     */
 
 // Version One
     /*
@@ -56,6 +72,7 @@ int main(int argc, const char * argv[]) {
 }
 
 // Version Two
+/*
 int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber) {
     int newNumber = previousNumber + previousPreviousNumber;
     if (remainingSteps == 0) {
@@ -66,5 +83,18 @@ int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previous
         remainingSteps--;
         return recursiveValueFibonacci(remainingSteps, previousNumber, previousPreviousNumber);
     }
-    
+ }
+ */
+
+// Version Three
+int recursiveValueFibonacci(int remainingSteps, int *previousNumber, int *previousPreviousNumber) {
+    int newNumber = *previousNumber + *previousPreviousNumber;
+    if (remainingSteps == 0) {
+        return newNumber;
+    } else {
+        *previousPreviousNumber = *previousNumber;
+        *previousNumber = newNumber;
+        remainingSteps--;
+        return recursiveValueFibonacci(remainingSteps, previousNumber, previousPreviousNumber);
+    }
 }
